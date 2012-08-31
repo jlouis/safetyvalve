69b941d5138f8d7e33df0b50352e142219061788
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2012-08-31 21:21:26 +0200

    Allow `safetyvalve_app` to correctly initialize queues.

--- src/safetyvalve_app.erl
+++ src/safetyvalve_app.erl
@@ -10,7 +10,17 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    safetyvalve_sup:start_link().
+    {ok, MainPid} = safetyvalve_sup:start_link(),
 
+    {ok, Queues} = application:get_env(safetyvalve, queues),
+    
+    [ok = start_queue(Q) || Q <- Queues],
+    {ok, MainPid}.
+    
 stop(_State) ->
+    ok.
+
+%% ----------------------------------------------------------------------
+start_queue({QName, Conf}) ->
+    {ok, _Pid} = safetyvalve_sup:start_queue(QName, Conf),
     ok.
