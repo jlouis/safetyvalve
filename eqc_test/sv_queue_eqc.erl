b5670ce679a8e7184e3b48ae744c181ab492ac65
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2012-09-04 19:04:16 +0200

    Capture all possible transition states currently possible.

--- eqc_test/sv_queue_eqc.erl
+++ eqc_test/sv_queue_eqc.erl
@@ -6,16 +6,19 @@
 %%
 %% * The concurrency level on the queue is 1.
 %% * The queue size is 1, so there are at most a single waiter.
+%% * The poll rate of the queue is 1 and maximum token count is 1.
 %%
 %% The postconditions we want to check are:
 %% * The concurrency level in the SUT is *never* more than 1.
 %% * The queue size in the SUT is *never* more than 1.
+%% * The maximum token count is *never* more than 1.
 %%
 %% So if we spawn a new process when the queue is full, we expect that
 %% new spawn to be denied queueing access since the queue is overloaded.
 %%
 %% We *do* want to generate random command sequences for our queue to
 %% check this however, hence we write a quickcheck test case for it.
+%%
 -module(sv_queue_eqc).
 
 -compile([export_all]).
@@ -23,15 +26,46 @@
 -include_lib("eqc/include/eqc_statem.hrl").
 -eqc_group_commands(true).
 
+%% The record state are 0/1 values on concurrency, queue size and
+%% tokens. These mandate when you can expect a certain command to be possible
+%% and also captures the possible transition states on the queue:
+%% {0, 0, 0} -> poll -> {0, 0, 1}
+%% {0, 0, 0} -> queue -> {0, 1, 0}
+
+%% {0, 0, 1} -> poll -> {0, 0, 1}
+%% {0, 0, 1} -> queue -> {0, 1, 1}
+
+%% {0, 1, 0} -> poll -> {1, 0, 0}
+%% {0, 1, 0} -> queue -> {0, 1, 0} (denied)
+%% {0, 1, 1} -> *impossible* - should immediately go to {1, 0, 0}
+
+%% {1, 0, 0} -> poll -> {1, 0, 1}
+%% {1, 0, 0} -> queue -> {1, 1, 0}
+%% {1, 0, 0} -> done -> {0, 0, 0}
+
+%% {1, 0, 1} -> poll -> {1, 0, 1}
+%% {1, 0, 1} -> queue -> {1, 1, 1}
+%% {1, 0, 1} -> done -> {0, 0, 1}
+
+%% {1, 1, 0} -> poll -> {1, 1, 1}
+%% {1, 1, 0} -> queue -> {1, 1, 0} (denied)
+%% {1, 1, 0} -> done -> {0, 1, 0}
+
+%% {1, 1, 1} -> poll -> {1, 1, 1}
+%% {1, 1, 1} -> queue -> {1, 1, 1} (denied)
+%% {1, 1, 1} -> done -> {1, 0, 0}
+%%
 -record(state,
         { concurrency,
-          queue_size }).
+          queue_size,
+          tokens }).
 
 %% The intial queue state
 %% ----------------------------------------------------------------------
 initial_state() ->
     #state { concurrency = 0,
-             queue_size  = 0 }.
+             queue_size  = 0,
+             tokens      = 1 %% Initialized to the rate of the queue }.
 
 %% POLLING OF THE QUEUE
 %% ----------------------------------------------------------------------
@@ -48,6 +82,9 @@ poll_command(_S) ->
 %% anything happening.
 poll_pre(_S) -> true.
 
+poll_next(#state { tokens = T} = State) ->
+    State#state { tokens = min(1, T+1) }.
+
 %% SPAWNING A NEW PROCESS
 %% ----------------------------------------------------------------------
 %% We may spawn a new worker who will try to go the queue for work.
@@ -59,23 +96,27 @@ spawn_new_empty_q_command() ->
     {call, ?MODULE, spawn_new_process, []}.
 
 %% We may only make a spawn on an empty queue, when the concurrency
-%% level is 0 and the queue size is 0.
+%% level is 0 and the queue size is 0. Furthermore, there should be a
+%% token we can consume.
 spawn_new_empty_q_pre(#state { concurrency = 0,
-                               queue_size  = 0 }) -> true;
+                               queue_size  = 0,
+                               tokens      = 1 }) -> true;
 spawn_new_empty_q_pre(_) -> false.
 
 %% When this call succeeds, you have a single worker doing work, and
 %% you still have a queue size which is empty.
 spawn_new_empty_q_next(#state { concurrency = 0,
                                 queue_size  = 0 } = State, _V, []) ->
-    State#state { concurrency = 1, queue_size = 0 }.
+    State#state { concurrency = 1, queue_size = 0, tokens = 0 }.
 
 %% Case 2: The there is a worker, so we will queue
 spawn_new_doing_work_command(_S) ->
     {call, ?MODULE, spawn_new_process, []}.
 
 %% We can only queue when there is a worker doing work already.
-spawn_new_doing_work_pre(#state { concurrency = 1, queue_size = 0}) -> true;
+spawn_new_doing_work_pre(#state { concurrency = 1,
+                                  queue_size = 0,
+                                  tokens = 1}) -> true;
 spawn_new_doing_work_pre(_) -> false.
 
 spawn_new_doing_work_next(#state { queue_size = 0 } = State, _V, []) ->
