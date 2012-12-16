20fa45cad934c9f68b6b4ff41ce7a6f393ff6c32
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2012-12-16 22:48:43 +0100

    Fix an error pertaining to unique timestamp generation.
    If you have a fast system, you can surely generate multiple entries on the same
    timestamp. This violates the assumption of the queue, so we change to use
    an `erlang:now()` timestmap for now. This fixes the problem, and later we can
    change into another kind of timestamp.

--- src/sv.erl
+++ src/sv.erl
@@ -31,8 +31,5 @@ timestamp() ->
 	%% Timestamps *have* to be unique. Calling erlang:now/0 makes sure
 	%% this happens. But you can use any ordered term if you want, for instance
 	%% {os:timestamp(), self()} or {os:timestamp(), ref()}.
-	{Mega, Secs, Micros} = erlang:now(),
-	Seconds = Mega * 1000000 + Secs,
-	Millis = Micros div 1000,
-	Seconds * 1000 + Millis.
+	erlang:now().
 
