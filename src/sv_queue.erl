b3f704b1143167dfc7b3532072c1061e9d0fd6bf
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2012-12-16 22:51:30 +0100

    Use the new ETS-based queue.

--- src/sv_queue.erl
+++ src/sv_queue.erl
@@ -35,7 +35,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -define(SERVER, ?MODULE). 
--define(QUEUE, queue).
+-define(QUEUE, sv_queue_ets).
 
 -record(conf, { hz, rate, token_limit, size, concurrency }).
 -record(state, {
