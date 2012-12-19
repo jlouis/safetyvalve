f1a275bf5db93946081c64f076450b07d342d659
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2012-12-19 20:41:17 +0100

    Extract dropped packets from FQ-codel.

--- src/sv_codel.erl
+++ src/sv_codel.erl
@@ -49,37 +49,38 @@ dodequeue(Now, #state {
 dequeue(Now, #state { dropping = Dropping } = State) ->
   case dodequeue(Now, State) of
     {nodrop, empty, NState} ->
-      {empty, NState#state { dropping = false }};
+      {empty_queue, NState#state { dropping = false }};
     {nodrop, Pkt, #state {} = NState} when Dropping ->
-      {Pkt, NState#state { dropping = false }};
+      {ok, Pkt, [], NState#state { dropping = false }};
     {drop, Pkt, #state { drop_next = DropNext } = NState} when Now >= DropNext ->
-      dequeue_drop_next(Now, Pkt, NState);
+      dequeue_drop_next(Now, Pkt, NState, []);
     {nodrop, Pkt, NState} when not Dropping ->
-      {Pkt, NState};
-    {drop, _Pkt, #state { drop_next = DN, interval = I, first_above_time = FirstAbove } = NState}
+      {ok, Pkt, [], NState};
+    {drop, Pkt, #state { drop_next = DN, interval = I, first_above_time = FirstAbove } = NState}
     	when not Dropping, Now - DN < I orelse Now - FirstAbove >= I ->
-        		dequeue_start_drop(Now, NState);
+        		dequeue_start_drop(Now, NState, [Pkt]);
     {drop, Pkt, NState} when not Dropping ->
-      {Pkt, NState}
+      {ok, Pkt, [], NState}
   end.
 
      
 
-dequeue_drop_next(Now, _DroppedPkt, #state { drop_next = DN, dropping = true, count = C } = State) ->
+dequeue_drop_next(Now, Candidate, #state { drop_next = DN, dropping = true, count = C } = State, Dropped) ->
     case dodequeue(Now, State#state { count = C+1 }) of
       {nodrop, Res, NState} ->
-        {Res, NState#state { dropping = false }};
+        {ok, Res, [Candidate | Dropped], NState#state { dropping = false }};
       {drop, Res, #state { count = NC, interval = I } = NState} ->
-        dequeue_drop_next(Now, Res, NState#state { drop_next = control_law(DN, I, NC) })
+        dequeue_drop_next(Now, Res, NState#state { drop_next = control_law(DN, I, NC) }, [Candidate | Dropped])
     end;
-dequeue_drop_next(_Now, Pkt, #state { dropping = false } = State) ->
-  {Pkt, State}.
+dequeue_drop_next(_Now, Candidate, #state { dropping = false } = State, Dropped) ->
+  {ok, Candidate, Dropped, State}.
 
-dequeue_start_drop(Now, #state { drop_next = DN, interval = Interval, count = Count } = State)
+dequeue_start_drop(Now, #state { drop_next = DN, interval = Interval, count = Count } = State, Dropped)
 	when Now - DN < Interval ->
+    {ok, Dropped, 
     State#state {
     	dropping = true,
     	count = case Count > 2 of true -> Count - 2; false -> 1 end,
-    	drop_next = control_law(Now, Interval, Count) };
-dequeue_start_drop(Now, #state { interval = Interval, count = Count } = State) ->
-    State#state { dropping = true, count = 1, drop_next = control_law(Now, Interval, Count) }.
+    	drop_next = control_law(Now, Interval, Count) }};
+dequeue_start_drop(Now, #state { interval = Interval, count = Count } = State, Dropped) ->
+    {ok, Dropped, State#state { dropping = true, count = 1, drop_next = control_law(Now, Interval, Count) }}.
