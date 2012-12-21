c15fcb2d8b83dfa9b93d6f09f18301b11f2364a6
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2012-12-21 21:13:40 +0100

    Introduce code to run the CoDel queue base.
    This eqc test will just run the queue aggressively in order to test that
    it really works like it was intended for it to work. This makes sure there
    are no ways by which we can force the queue to crash.
    
    In other words, we just proved a termination property.

--- eqc_test/sv_codel_eqc.erl
+++ eqc_test/sv_codel_eqc.erl
@@ -3,17 +3,15 @@
 -compile([export_all]).
 
 -include_lib("eqc/include/eqc.hrl").
--eqc_group_commands(true).
 
--record(state,
+-record(model,
     { ttd = 10,
       t = 0,
       st
     }).
     
-g_time_advance(#state { ttd = N, t = T } = State) ->
-	?LET(K, choose(0, N),
-		State#state { ttid = N - K, t = T + K }).
+g_time_advance() ->
+    choose(1, 10).
     
 g_model(0, todo) ->
 	oneof([{call, ?MODULE, new, []}]);
@@ -22,23 +20,43 @@ g_model(N, todo) ->
 		{1, g_model(0, todo)},
 		{N, ?LET(M, g_model(max(0, N-2), todo),
 		    frequency(
-		        [{200, {call, ?MODULE, advance_time, M, g_time_advance(M)}} || not boundary(M)] ++
-		        [{200, {call, ?MODULE, enqueue, [M]}}] ++
-		        [{100, {call, ?MODULE, dequeue, [M]}} || boundary(M)]
-		        ])}]).
+		        [{100, {call, ?MODULE, advance_time,
+		            [M, g_time_advance()]}} || not boundary(M)] ++
+		        [{100, {call, ?MODULE, enqueue, [M]}}] ++
+		        [{100, {call, ?MODULE, dequeue, [M]}} || boundary(M)]))}]).
+		        
 
-boundary(#state { ttd = 0 }) -> true;
+g_model() ->
+    ?SIZED(Size, g_model(Size, todo)).
+
+boundary(#model { ttd = 0 }) -> true;
 boundary(_) -> false.
+
+%% Properties
+%% ----------------------------------------------
+
+%% Verify that the queue runs if we blindly execute it
+prop_termination() ->
+    ?FORALL(M, g_model(),
+    	begin
+    		_R = eval(M),
+    		true
+    	end).
+
 
 %% Operations
 %% ----------------------------------------------
 
 new() ->
-	#state { ttd = 10, t = 0, st = sv_codel:init() }.
+	#model { ttd = 10, t = 0, st = sv_codel:init() }.
 
-enqueue(#state { t = T, st = ST } = State) ->
-	State#state { t = T+1, st = sv_codel:enqueue({pkt, T}, T, ST) }.
+advance_time(#model { t = T, ttd = TTD } = State, K) ->
+    Inc = min(K, TTD),
+    State#model { t = T + Inc, ttd = TTD - Inc }.
+
+enqueue(#model { t = T, st = ST } = State) ->
+	State#model { t = T+1, st = sv_codel:enqueue({pkt, T}, T, ST) }.
 	
-dequeue(#state { t = T, st = ST }) ->
+dequeue(#model { t = T, st = ST } = State) ->
 	{_, ST2} = sv_codel:dequeue(T, ST),
-	State#state { t = T+1, ttd = 10, st = ST2 }.
+	State#model { t = T+1, ttd = 10, st = ST2 }.
