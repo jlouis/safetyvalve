6835e210ed8db2c619b9810c4b23cc7c11441f44
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2012-12-21 22:53:23 +0100

    Generalize test model more and enable correct dequeueing.
    Begin adding more properties in there for the CoDel code.

--- eqc_test/sv_codel_eqc.erl
+++ eqc_test/sv_codel_eqc.erl
@@ -5,11 +5,11 @@
 -include_lib("eqc/include/eqc.hrl").
 
 -record(model,
-    { ttd = 10,
-      t = 0,
-      st
-    }).
-    
+    { t = 0, st }).
+
+g_cmd_advance_time(M) ->
+    {call, ?MODULE, advance_time, [M, g_time_advance()]}.
+
 g_time_advance() ->
     choose(1, 10).
     
@@ -20,17 +20,13 @@ g_model(N, todo) ->
 		{1, g_model(0, todo)},
 		{N, ?LET(M, g_model(max(0, N-2), todo),
 		    frequency(
-		        [{400, {call, ?MODULE, advance_time,
-		            [M, g_time_advance()]}} || not boundary(M)] ++
+		        [{400, g_cmd_advance_time(M)}] ++
 		        [{100, {call, ?MODULE, enqueue, [M]}}] ++
-		        [{200, {call, ?MODULE, dequeue, [M]}} || boundary(M)]))}]).
+		        [{200, {call, ?MODULE, dequeue, [M]}}]))}]).
 		        
 
 g_model() ->
     ?SIZED(Size, g_model(Size, todo)).
-
-boundary(#model { ttd = 0 }) -> true;
-boundary(_) -> false.
 
 %% Properties
 %% ----------------------------------------------
@@ -46,31 +42,38 @@ prop_termination() ->
 %% If the queue is empty, we are never in a dropping state
 prop_empty_q_no_drop() ->
     ?FORALL(M, g_model(),
-        begin
+	begin
             #model { t = T, st = ST} = eval(M),
             case sv_codel:dequeue(T+1, ST) of
                 {empty, _Dropped, EmptyState} ->
                     PL = sv_codel:qstate(EmptyState),
-                    not proplists:get_value(dropping, PL)
+                    (not proplists:get_value(dropping, PL))
                       andalso proplists:get_value(first_above_time, PL) == 0;
+                {ok, _Pkt, [_ | _], CoDelState} ->
+                    PL = sv_codel:qstate(CoDelState),
+                    %% We dropped packets, our state must be dropping
+                    proplists:get_value(dropping, PL);
                 {ok, _Pkt, _Dropped, _SomeState} ->
                     true
-            end
-        end).
+             end
+        	end).
 
 %% Operations
 %% ----------------------------------------------
 
 new() ->
-	#model { ttd = 10, t = 0, st = sv_codel:init() }.
+	#model { t = 0, st = sv_codel:init() }.
 
-advance_time(#model { t = T, ttd = TTD } = State, K) ->
-    Inc = min(K, TTD),
-    State#model { t = T + Inc, ttd = TTD - Inc }.
+advance_time(#model { t = T } = State, K) ->
+    State#model { t = T + K  }.
 
 enqueue(#model { t = T, st = ST } = State) ->
 	State#model { t = T+1, st = sv_codel:enqueue({pkt, T}, T, ST) }.
 	
 dequeue(#model { t = T, st = ST } = State) ->
-	{_, ST2} = sv_codel:dequeue(T, ST),
-	State#model { t = T+1, ttd = 10, st = ST2 }.
+    ST2 =
+    	case sv_codel:dequeue(T, ST) of
+    		{ok, _, _, S} -> S;
+    		{empty, _, S} -> S
+    	end,
+    State#model { t = T+1, st = ST2 }.
