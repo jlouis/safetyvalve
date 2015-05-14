0de5cbdfe7f81f677ee3493a209cf700de7184b1
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2015-05-14 13:30:44 +0200

    Revive the CT test suite, make sure all tests pass.

--- test/sv_SUITE.erl
+++ test/sv_SUITE.erl
@@ -21,17 +21,22 @@ end_per_group(_Group, _Config) ->
     ok.
 
 init_per_suite(Config) ->
-    [ok = application:start(App) ||
-        App <- [syntax_tools, compiler, lager, safetyvalve]],
+    application:load(safetyvalve),
+    Queues = [
+      {test_queue_1_ets, [{hz, 1000}, {rate, 5}, {token_limit, 15}, {size, 60}, {concurrency, 3}]},
+      {test_queue_1_codel, [{hz, 200}, {rate, 1}, {token_limit, 15}, {size, 60}, {concurrency, 3},
+      	{queue_type, {sv_codel, [5, 100]}}]}
+    ],
+    application:set_env(safetyvalve, queues, Queues),
+    {ok, _Apps} = application:ensure_all_started(safetyvalve),
     Config.
 
 end_per_suite(_Config) ->
-    [ok = application:stop(App) ||
-        App <- lists:reverse([syntax_tools, compiler, lager, safetyvalve])],
+    application:stop(safetyvalve),
     ok.
 
 init_per_testcase(many_through_codel, Config) ->
-    sv_tracer:start_link(filename:join(?config(priv_dir, Config), "trace.out")),
+    %% sv_tracer:start_link(filename:join(?config(priv_dir, Config), "trace.out")),
     Config;
 init_per_testcase(not_applicable, Config) ->
     dbg:tracer(),
